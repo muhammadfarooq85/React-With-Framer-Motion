a0488029201fd10a435c73086323b82537ee3a8b
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2025-08-03 09:49:05 +0500

    battery animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
-"use client";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
-import { animate, motion, useMotionValue, useTransform } from "motion/react";
-import { useEffect } from "react";
+export default function App() {
+  const [scale, setScale] = useState(0.1);
 
-export default function HTMLContent() {
-  const count = useMotionValue(0);
-  const rounded = useTransform(() => Math.round(count.get()));
-
-  useEffect(() => {
-    const controls = animate(count, 100, { duration: 5 });
-    return () => controls.stop();
-  }, []);
-
-  return <motion.pre style={{fontSize:100 }} transition={{
-    duration:5
-  }}>{rounded}</motion.pre>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap:10,
+        alignItems: "center",
+        height: "150vh",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#212121",
+          width: 150,
+          height: 300,
+          borderRadius: 20,
+        }}
+      >
+        <motion.div
+          style={{
+            backgroundColor: "#fff537",
+            width: "100%",
+            height: "100%",
+            scaleY: scale,
+            borderRadius: 20,
+            transformOrigin: "bottom",
+          }}
+          whileInView={{
+            opacity: 1,
+          }}
+          transition={{ duration: 5 }}
+        ></motion.div>
+      </div>
+      <div>
+        <button onClick={() => setScale(0.2)}>Scale Up</button>
+        <button onClick={() => setScale(0.3)}>Scale Up</button>
+        <button onClick={() => setScale(0.4)}>Scale Up</button>
+        <button onClick={() => setScale(0.5)}>Scale Up</button>
+        <button onClick={() => setScale(0.6)}>Scale Up</button>
+        <button onClick={() => setScale(0.7)}>Scale Up</button>
+        <button onClick={() => setScale(0.8)}>Scale Up</button>
+        <button onClick={() => setScale(0.9)}>Scale Up</button>
+        <button onClick={() => setScale(1)}>Scale Up</button>
+      </div>
+    </div>
+  );
 }
