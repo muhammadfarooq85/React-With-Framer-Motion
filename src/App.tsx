d08b7e039f0dedcb815ebdf2c52fb649cf982783
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2025-08-03 10:11:59 +0500

    spring animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,60 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 export default function App() {
-  const [scale, setScale] = useState(0.1);
-
   return (
     <div
       style={{
         display: "flex",
-        flexDirection: "column",
         justifyContent: "center",
-        gap:10,
         alignItems: "center",
-        height: "150vh",
+        gap: 10,
       }}
     >
-      <div
+      <motion.div
         style={{
-          backgroundColor: "#212121",
-          width: 150,
-          height: 300,
-          borderRadius: 20,
+          borderRadius: 10,
+          width: 40,
+          height: 40,
+          backgroundColor: "#fff",
         }}
-      >
-        <motion.div
-          style={{
-            backgroundColor: "#fff537",
-            width: "100%",
-            height: "100%",
-            scaleY: scale,
-            borderRadius: 20,
-            transformOrigin: "bottom",
-          }}
-          whileInView={{
-            opacity: 1,
-          }}
-          transition={{ duration: 5 }}
-        ></motion.div>
-      </div>
-      <div>
-        <button onClick={() => setScale(0.2)}>Scale Up</button>
-        <button onClick={() => setScale(0.3)}>Scale Up</button>
-        <button onClick={() => setScale(0.4)}>Scale Up</button>
-        <button onClick={() => setScale(0.5)}>Scale Up</button>
-        <button onClick={() => setScale(0.6)}>Scale Up</button>
-        <button onClick={() => setScale(0.7)}>Scale Up</button>
-        <button onClick={() => setScale(0.8)}>Scale Up</button>
-        <button onClick={() => setScale(0.9)}>Scale Up</button>
-        <button onClick={() => setScale(1)}>Scale Up</button>
-      </div>
+        animate={{
+          y: 300,
+        }}
+        transition={{
+          type: "spring",
+          duration: 2,
+        }}
+      ></motion.div>
+      <motion.div
+        style={{
+          borderRadius: 10,
+          width: 40,
+          height: 40,
+          backgroundColor: "#fff",
+        }}
+        animate={{
+          y: 300,
+        }}
+        transition={{
+          type: "spring",
+          duration: 3,
+        }}
+      ></motion.div>
+      <motion.div
+        style={{
+          borderRadius: 10,
+          width: 40,
+          height: 40,
+          backgroundColor: "#fff",
+        }}
+        animate={{
+          y: 300,
+        }}
+        transition={{
+          type: "spring",
+          duration: 4,
+        }}
+      ></motion.div>
     </div>
   );
 }
